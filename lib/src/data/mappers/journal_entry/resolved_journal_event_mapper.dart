c334e139574178398575128b81707ba3d2f85c58
Tatyana Korotysh <tatsiana.karatysh@innowise-group.com>
2023-08-28 08:00:48 +0200

    add resolved_journal_event_mapper.dart

---
+++ lib/src/data/mappers/journal_entry/resolved_journal_event_mapper.dart
@@ -0,0 +1,11 @@
+import '../../../domain/domain.dart';
+
+class ResolvedJournalEventMapper {
+  static ResolvedJournalEvent fromJson(Map<String, dynamic> map) {
+    return ResolvedJournalEvent(
+      id: map["id"] as String,
+      publicName: map["publicName"] as String,
+      iconName: map["iconName"] as String,
+    );
+  }
+}
