187244c491745b76abd5c370ed4ad87f8450ab00
Tatyana Korotysh <tatsiana.karatysh@innowise-group.com>
2023-11-10 14:50:12 +0100

    refactor callMethodChannel method ans return getting mock data in _safeRequest method

--- lib/src/data/core/sdk_method_channel.dart
+++ lib/src/data/core/sdk_method_channel.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 
 import '../../domain/domain.dart';
 import '../consts/method_channel_consts.dart';
+import '../mock/mock_data_provider.dart';
 import 'sdk_method_channel_core.dart';
 
 /// An implementation of [SDKMethodChannelCore] that uses method channels.
@@ -15,27 +16,25 @@ class SDKMethodChannel extends SDKMethodChannelCore {
   Future<T> callMethodChannel<T>({
     required String method,
     Map<String, dynamic>? params,
+    // //param for mock data
+    MetricType? metricType,
     MethodChannel? methodChannel,
   }) async {
-    final T response;
-
-    if (methodChannel != null) {
-      response = await _safeRequest(
-        request: () => methodChannel.invokeMethod<T>(method, params),
-      );
-    } else {
-      response = _safeRequest(
-        request: () async => _methodChannel.invokeMethod<T>(method, params),
-      );
-    }
-
-    return response;
+    return await _safeRequest(
+      request: () => methodChannel != null
+          ? methodChannel.invokeMethod<T>(method, params)
+          : _methodChannel.invokeMethod<T>(method, params),
+      method: method,
+      metricType: metricType,
+    );
   }
 
   Stream<dynamic> callEventChannel({
     required String method,
     required EventChannel eventChannel,
     Map<String, dynamic>? params,
+    //param for mock data
+    MetricType? metricType,
   }) {
     final Map<String, dynamic> data = <String, dynamic>{
       'method': method,
@@ -46,15 +45,19 @@ class SDKMethodChannel extends SDKMethodChannelCore {
     }
 
     return _safeRequest(
+      method: method,
       request: () => eventChannel.receiveBroadcastStream(data),
+      metricType: metricType,
     );
   }
 
   dynamic _safeRequest({
     required Function() request,
+    //params for mock data
+    required String method,
+    MetricType? metricType,
   }) {
-    if (defaultTargetPlatform == TargetPlatform.android ||
-        defaultTargetPlatform == TargetPlatform.iOS) {
+    if (defaultTargetPlatform == TargetPlatform.android) {
       try {
         return request();
       } on PlatformException catch (e) {
@@ -63,6 +66,11 @@ class SDKMethodChannel extends SDKMethodChannelCore {
           reason: e.details.toString(),
         );
       }
+    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+      return MockDataProvider.callMockMethod(
+        method: method,
+        metricType: metricType,
+      );
     } else {
       throw QAError(
         description: 'QAFlutterPlugin is not implemented for ${Platform.operatingSystem}',
