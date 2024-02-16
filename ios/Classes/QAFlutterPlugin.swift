f9bc291dc34518270f12a9a54c9fd26674315d8d
Tatsiana-Karatysh <tatsiana.karatysh@innowise-group.com>
2024-02-16 14:58:23 +0100

    fix init ios method channels

--- ios/Classes/QAFlutterPlugin.swift
+++ ios/Classes/QAFlutterPlugin.swift
@@ -5,20 +5,24 @@ import QuantActionsSDK
 public class QAFlutterPlugin: NSObject, FlutterPlugin {
     
     public static func register(with registrar: FlutterPluginRegistrar) {
+        DataCollectionRunningMethodChannel.register(with: registrar)
+        UserEventChannel.register(with: registrar)
         VoidMethodChannel.register(with: registrar)
         DeviceMethodChannel.register(with: registrar)
         IsDeviceRegisteredMethodChannel.register(with: registrar)
+        IsKeyboardAddedMethodChannel.register(with: registrar)
+        KeyboardSettingsMethodChannel.register(with: registrar)
         SubscribeMethodChannel.register(with: registrar)
         SubscriptionMethodChannel.register(with: registrar)
-        IsKeyboardAddedMethodChannel.register(with: registrar)
+        UpdateKeyboardSettingsMethodChannelregister(with: registrar)
         GetJournalEntryMethodChannel.register(with: registrar)
-        SaveJournalEntryMethodChannel.register(with: registrar)
-        BasicInfoMethodChannel.register(with: registrar)
-        InitMethodChannel.register(with: registrar)
         GetJournalEventKindsMethodChannel.register(with: registrar)
+        SaveJournalEntryMethodChannel.register(with: registrar)
+        GetBasicInfoMethodChannel.register(with: registrar)
+        InitMethodChannel.register(with: registrar)
         
-        MetricAndTrendEventChannel.register(with: registrar)
         GetJournalEntitiesEventChannel.register(with: registrar)
+        MetricAndTrendEventChannel.register(with: registrar)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {}
