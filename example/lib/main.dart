5102933fddf620e6e139d339b7d65dad79033caf
Enea Ceolini <enea.ceolini@quantactions.com>
2024-03-29 14:42:02 +0100

    feat: cleanup example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:qa_flutter_plugin/qa_flutter_plugin.dart';
 
 void main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -15,9 +21,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static const String tempApiKey = "55b9cf50-dac2-11e6-b535-fd8dff3bf4e9";
+
+
+  static final String tempApiKey = dotenv.env['qa_sdk_api_key'] ?? '';
+  // check the platform type
+
+  final targetPlatform = defaultTargetPlatform;
+
   final _qa = QAFlutterPlugin();
-  late Stream<TimeSeries<dynamic>> _stream;
+  late Stream<TimeSeries<dynamic>> _stream1;
+  late Stream<TimeSeries<dynamic>> _stream2;
+  late Stream<TimeSeries<dynamic>> _stream3;
 
   String? errorText;
 
@@ -36,7 +50,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('QuantActions Plugin Example'),
         ),
         body: errorText != null
             ? Text(errorText!)
@@ -44,10 +58,44 @@ class _MyAppState extends State<MyApp> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+
+                    targetPlatform == TargetPlatform.android
+                        ? const Text('Platform: Android')
+                        : targetPlatform == TargetPlatform.iOS
+                            ? const Text('Platform: iOS')
+                            : const Text('Platform: Web'),
+
+                    Text('Cognitive Fitness:', style: Theme.of(context).textTheme.headline6),
                     SizedBox(
                       height: 200,
                       child: StreamBuilder(
-                        stream: _stream,
+                        stream: _stream1,
+                        builder: (_, AsyncSnapshot<TimeSeries<dynamic>> snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView.builder(
+                              itemCount: snapshot.data!.timestamps.length,
+                              itemBuilder: (_, int index) {
+                                // final DateTime item = snapshot.data!.timestamps[index];
+                                final int item = snapshot.data!.values[index].toInt();
+                                final date = DateFormat.yMMMd().format(snapshot.data!.timestamps[index]);
+                                return Text('$date: $item');
+                              },
+                            );
+                          }
+
+                          if (snapshot.hasError) {
+                            return Text('error: ${snapshot.error.toString()}');
+                          }
+
+                          return Text('status: ${snapshot.connectionState.name}');
+                        },
+                      ),
+                    ),
+                    Text('Screen Time:', style: Theme.of(context).textTheme.headline6),
+                    SizedBox(
+                      height: 200,
+                      child: StreamBuilder(
+                        stream: _stream2,
                         builder: (_, AsyncSnapshot<TimeSeries<dynamic>> snapshot) {
                           if (snapshot.hasData) {
                             return ListView.builder(
@@ -55,7 +103,34 @@ class _MyAppState extends State<MyApp> {
                               itemBuilder: (_, int index) {
                                 // final DateTime item = snapshot.data!.timestamps[index];
                                 final ScreenTimeAggregate item = snapshot.data!.values[index];
-                                return Text(item.totalScreenTime.toString());
+                                final timeInMinutes = item.totalScreenTime / 1000 / 60;
+                                final date = DateFormat.yMMMd().format(snapshot.data!.timestamps[index]);
+                                return Text('$date: ${timeInMinutes.toInt()} minutes');
+                              },
+                            );
+                          }
+
+                          if (snapshot.hasError) {
+                            return Text('error: ${snapshot.error.toString()}');
+                          }
+
+                          return Text('status: ${snapshot.connectionState.name}');
+                        },
+                      ),
+                    ),
+                    Text('Sleep Summary:', style: Theme.of(context).textTheme.headline6),
+                    SizedBox(
+                      height: 200,
+                      child: StreamBuilder(
+                        stream: _stream3,
+                        builder: (_, AsyncSnapshot<TimeSeries<dynamic>> snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView.builder(
+                              itemCount: snapshot.data!.timestamps.length,
+                              itemBuilder: (_, int index) {
+                                // final DateTime item = snapshot.data!.timestamps[index];
+                                final SleepSummary item = snapshot.data!.values[index];
+                                return Text('Sleep start: ${item.sleepStart}');
                               },
                             );
                           }
@@ -76,10 +151,24 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _initDependencies() async {
-    _stream = _qa.getMetricSample(
+
+    _stream1 = _qa.getMetricSample(
+      apiKey: tempApiKey,
+      metric: Metric.cognitiveFitness,
+      interval: MetricInterval.day,
+    );
+
+    _stream2 = _qa.getMetricSample(
       apiKey: tempApiKey,
       metric: Metric.screenTimeAggregate,
       interval: MetricInterval.day,
     );
+
+    _stream3 = _qa.getMetricSample(
+      apiKey: tempApiKey,
+      metric: Metric.sleepSummary,
+      interval: MetricInterval.day,
+    );
+
   }
 }
