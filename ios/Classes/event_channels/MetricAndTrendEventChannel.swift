8dc5a6881d8423a1eaffb7c192465e431952786b
Enea Ceolini <enea.ceolini@quantactions.com>
2024-05-23 14:51:04 +0200

    fix: updated how trends is checked for nans

--- ios/Classes/event_channels/MetricAndTrendEventChannel.swift
+++ ios/Classes/event_channels/MetricAndTrendEventChannel.swift
@@ -53,6 +53,7 @@ class MetricAndTrendEventChannel : NSObject, FlutterStreamHandler {
         // TODO - remove this!!!
 //        participationID = "enea-test-id";
 //        participationID = "8ff6b05d-6b3c-4833-9e2f-d40f2fa88543";
+//        participationID = "138ef231354cb7994b3c925b50a89578793f28eb";
         
         
         let metric = params?["metric"] as? String
--- lib/src/domain/models/trend_holder/trend_holder_extension.dart
+++ lib/src/domain/models/trend_holder/trend_holder_extension.dart
@@ -4,13 +4,7 @@ part of 'trend_holder.dart';
 extension TrendHolderExtension on TrendHolder {
   bool get isNaN => difference2Weeks.isNaN &&
         difference6Weeks.isNaN &&
-        difference1Year.isNaN &&
-        statistic2Weeks.isNaN &&
-        statistic6Weeks.isNaN &&
-        statistic1Year.isNaN &&
-        significance2Weeks.isNaN &&
-        significance6Weeks.isNaN &&
-        significance1Year.isNaN;
+        difference1Year.isNaN;
 }
 
 
